58f2fff29052f72aaed3ab2f7ee46335f40e9201
pbarzaghi <pablobarzaghi@gmail.com>
2023-11-16 15:39:11 -0300

    mapeo de la clase Review.java

--- src/main/java/ar/programa/provices/model/entity/Review.java
+++ src/main/java/ar/programa/provices/model/entity/Review.java
@@ -1,11 +1,26 @@
 package ar.programa.provices.model.entity;
 
+import jdk.jfr.Enabled;
+
+import javax.persistence.*;
+
 @lombok.Data
 
 @lombok.NoArgsConstructor
+@Entity
+@Table (name = "review")
 public class Review {
+    @Id
+    @Column (name ="idCalificacion" )
+    @GeneratedValue (strategy=GenerationType.SEQUENCE, generator="ID_SEQ")
     int id;
-    int puntaje;
+
+    @Column (name ="puntaje" )
+    String puntaje;
+
+    @Column (name ="opinion" )
     String opimion;
+
+
 
 }
